1760433000ce2b113c95c5ba8cece8faf6bf1754
Sarah Peterson <sarahp@nurelm.com>
2020-10-11 11:28:08 -0400

    adds ids to person object for editing

--- src/components/peopleTable/peopleData.js
+++ src/components/peopleTable/peopleData.js
@@ -1,5 +1,6 @@
 export const people = [
   {
+    id: 0,
     name: 'Ann Henry',
     birthday: '04/12/1990',
     title: 'Product Manager',
@@ -7,6 +8,7 @@ export const people = [
     salary: '60,000 USD'
   },
   {
+    id: 1,
     name: 'Annette Williamson',
     birthday: '20/04/1985',
     title: 'CEO',
@@ -14,6 +16,7 @@ export const people = [
     salary: '60,000 USD'
   },
   {
+    id: 2,
     name: 'Philip Alexander',
     birthday: '25/07/1989',
     title: 'Software Engineer',

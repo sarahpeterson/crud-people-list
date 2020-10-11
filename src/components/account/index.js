a52b143b037ade865258b7381605724394582fa6
Sarah Peterson <sarahp@nurelm.com>
2020-10-11 13:30:03 -0400

    refactors css for account components

--- src/components/account/index.js
+++ src/components/account/index.js
@@ -1,52 +1,14 @@
 import React from 'react';
+import './styles.css';
 
 export default ({ account }) => (
-  <div
-    style={{
-      display: 'flex',
-      justifyContent: 'flex-end',
-      // alignItems: 'center',
-      flexDirection: 'row',
-      // position: 'relative',
-      // width: '100%',
-      backgroundColor: '#fff',
-      padding: 10
-    }}
-  >
-    <div
-      style={{
-        // position: 'absolute',
-        // left: 0,
-        // right: 0,
-        // top: 0,
-        // bottom: 0,
-        background: '#FFE8DF',
-        width: 36,
-        height: 36,
-        borderRadius: 36/2
-      }}
-    />
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        marginLeft: 20
-      }}
-    >
-      <p
-        style={{
-          margin: 0,
-          padding: 0
-        }}
-      >
+  <div className="account-container">
+    <div className="account-icon" />
+    <div className="text-section">
+      <p className="account-text">
         {account.name}
       </p>
-      <p
-        style={{
-          margin: 0,
-          padding: 0
-        }}
-      >
+      <p className="account-text">
         {account.type}
       </p>
     </div>

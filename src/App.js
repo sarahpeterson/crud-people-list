2f348fad13f1fa817412664cd36e6ea085f42f83
Sarah Peterson <sarahp@nurelm.com>
2020-10-11 11:29:58 -0400

    adds props for editing person to edit component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import PeopleTable from './components/peopleTable';
 import Edit from './components/edit';
+import {people} from './components/peopleTable/peopleData';
 
 function App() {
-  const [people, showPeople] = useState(true);
-  const [add, addPerson] = useState(true);
-  const [edit, editPerson] = useState(false);
+  const [newList, updatePeople] = useState(people);
+  const [add, addPerson] = useState(false);
+  const [edit, editPerson] = useState('');
   return (
     <div style={{ backgroundColor: '#F4F7FC', height: '200vh' }}>
-    {add ? <Edit edit={edit} /> :
-      <PeopleTable
-        btnClick={() => addPerson(true)}
+    {add || edit !== '' ?
+      <Edit
         edit={() => editPerson(true)}
+        editing={edit}
+        current={newList}
+        btnClick={(newPerson) => {
+          updatePeople(newPerson)
+          addPerson(false);
+          editPerson('')
+        }}
+      /> :
+      <PeopleTable
+        newPeople={newList}
+        btnClick={() => addPerson(true)}
+        edit={(id) => editPerson(id)}
       />}
     </div>
   );

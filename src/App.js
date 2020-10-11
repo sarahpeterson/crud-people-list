f39ce9d825449edac943cba93f27a279ef6f2fb2
Sarah Peterson <sarahp@nurelm.com>
2020-10-11 13:35:52 -0400

    updates app styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PeopleTable from './components/peopleTable';
 import Edit from './components/edit';
-import {people} from './components/peopleTable/peopleData';
+import { people } from './components/peopleTable/PeopleData';
 
 function App() {
   const [newList, updatePeople] = useState(people);
   const [add, addPerson] = useState(false);
   const [edit, editPerson] = useState('');
   return (
-    <div style={{ backgroundColor: '#F4F7FC', height: '200vh' }}>
-    {add || edit !== '' ?
-      <Edit
-        edit={() => editPerson(true)}
-        editing={edit}
-        current={newList}
-        btnClick={(newPerson) => {
-          updatePeople(newPerson)
-          addPerson(false);
-          editPerson('')
-        }}
-      /> :
+    <div className="App">
+      {add || edit !== '' ?
+        <Edit
+          edit={() => editPerson(true)}
+          editing={edit}
+          current={newList}
+          btnClick={(newPerson) => {
+            updatePeople(newPerson)
+            addPerson(false);
+            editPerson('')
+          }}
+        /> :
       <PeopleTable
         newPeople={newList}
         btnClick={() => addPerson(true)}

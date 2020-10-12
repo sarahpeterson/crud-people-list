822af42ef2a98711883387f65de36844d4219bb0
Sarah Peterson <sarahp@nurelm.com>
2020-10-12 18:24:23 -0400

    creates select list for country option

--- src/components/edit/Input.js
+++ src/components/edit/Input.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import CountryList from './CountryList';
 import './styles.css';
 
 export default ({ labelTxt, inputValue, btnClick, inputTxt, placeholder }) => {
+  if (labelTxt === 'Country') {
+    return (
+      <CountryList
+        value={inputValue}
+        btnClick={(e) => btnClick(e)}
+      />
+    )
+  }
   return (
     <div className="input-container">
       <label className="input-label">
         {labelTxt}
       </label>
       <input
+        data-testid="people-input"
         className="input-border"
         placeholder={placeholder}
         value={inputValue}

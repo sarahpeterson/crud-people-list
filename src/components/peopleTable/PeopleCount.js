a1e014d7d3626ea87abd23902893d5fe2ec66d04
Sarah Peterson <sarahp@nurelm.com>
2020-10-18 13:06:52 -0400

    moves people count component and styling to people table folder where its used

--- src/components/peopleTable/PeopleCount.js
+++ src/components/peopleTable/PeopleCount.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './styles.css';
 
 export default ({ count }) => (
-  <div className="count-container">
+  <span className="count-container">
     <p className="count-text">
       {count} employees
     </p>
-  </div>
+  </span>
 )
